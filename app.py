93f28637f9ef6fa1b3b4077f864bfdfc63485567
Parv Patel <patelparv1910@gmail.com>
2026-01-20 00:04:13 +0530

    Local host running

--- app.py
+++ app.py
@@ -30,3 +30,8 @@ def check_url():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+#Local Host Running
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
